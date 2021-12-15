a35e2f676cdfe58fd4ba6883b6f1980a59f172ad
Kapil Garg <kapil1garg@gmail.com>
2021-12-15 04:41:42 -0600

    adding day 7

--- 07/day7.py
+++ 07/day7.py
@@ -3,6 +3,7 @@ This module provides a solution for Advent of Code, Day 7: The Treachery of Whal
 For more information, see: https://adventofcode.com/2021/day/7.
 """
 from statistics import mean
+from math import inf
 import sys
 import os
 
@@ -23,13 +24,14 @@ def parse_input(input_list):
 
 
 def day7_pt1(input_file):
-    """<description>
+    """Computes the minimum cost to align crabs on a single point.
+    Cost function is linear with the shift each crab needs to make to the target position.
 
     Args:
         input_file (string): filename to load data from.
 
     Returns:
-        (type): <description>.
+        (int): minimum cost to align all crabs.
     """
     # get input from file
     inputs = read_input.read_lines_from_file(input_file)
@@ -41,43 +43,47 @@ def day7_pt1(input_file):
     min_pos = min(parsed_input)
     max_pos = max(parsed_input)
 
-    min_cost = None
+    # compute cost for each position that crabs can be aligned to
+    min_cost = inf
     for target_pos in range(min_pos, max_pos + 1):
+        # cost is linear shift from current position to target position
         curr_cost = sum(map(lambda x: abs(x - target_pos), parsed_input))
 
-        if min_cost == None or curr_cost < min_cost:
-            min_cost = curr_cost
+        # update cost
+        min_cost = min(min_cost, curr_cost)
 
     return min_cost
 
 
 def day7_pt2(input_file):
-    """<description>
+    """Computes the minimum cost to align crabs on a single point.
+    Cost function is arithmetic sequence from 1 to shift amount with step size of 1.
 
     Args:
         input_file (string): filename to load data from.
 
     Returns:
-        (type): <description>.
+        (int): minimum cost to align all crabs.
     """
     # get input from file
     inputs = read_input.read_lines_from_file(input_file)
 
-     # get all commands
+    # get all commands
     parsed_input = parse_input(inputs)
 
     # get min and max positions
     min_pos = min(parsed_input)
     max_pos = max(parsed_input)
 
-    min_cost = None
+    # compute cost for each position that crabs can be aligned to
+    min_cost = inf
     for target_pos in range(min_pos, max_pos + 1):
-        curr_cost = sum(map(
-            lambda x: mean([1, abs(x - target_pos)]) * abs(x - target_pos), parsed_input))
-        curr_cost = int(curr_cost)
+        # cost is arithmetic sequence from 1 to shift size
+        curr_cost = int(sum(map(
+            lambda x: mean([1, abs(x - target_pos)]) * abs(x - target_pos), parsed_input)))
 
-        if min_cost == None or curr_cost < min_cost:
-            min_cost = curr_cost
+        # update cost
+        min_cost = min(min_cost, curr_cost)
 
     return min_cost
 
